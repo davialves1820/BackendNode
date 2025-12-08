b3a6273128dffa77dfbbe45a41b46098d2d2b9b4
Davi Alves Rodrigues <davialvesr18@gmail.com>
2025-12-08 09:44:49 -0300

    Done User controllers

---
+++ src/app/controllers/userController.js
@@ -0,0 +1,203 @@
+import * as Yup from 'yup';
+import { Op } from 'sequelize';
+import { ParseIso } from "date-fns";
+import User from '../models/User';
+
+class UserController {
+    // List all customers
+        async index(req, res) {
+            const {name, email, createdBefore, createdAfter, updatedBefore, updatedAfter, sort} = req.query;
+    
+            const page = req.query.page ? parseInt(req.query.page) : 1;
+            const limit = req.query.limit ? parseInt(req.query.limit) : 25;
+            
+            let where = {};
+            let order = [];
+    
+            if (name) {
+                where = { ...where, name: {
+                    [Op.iLike]: name,
+                } };
+            }
+    
+            if (email) {
+                where = { ...where, email: {
+                    [Op.iLike]: email,
+                } };
+            }
+    
+            if (createdBefore) {
+                where = { ...where, createdAt: {
+                    [Op.lte]: ParseIso(createdBefore),
+                } };
+            }
+    
+            if (createdAfter) {
+                where = { ...where, createdAt: {
+                    [Op.gte]: ParseIso(createdAfter),
+                } };
+            }
+    
+            if (updatedBefore) {
+                where = { ...where, updateAt: {
+                    [Op.gte]: ParseIso(updatedBefore),
+                } };
+            }
+    
+            if (updatedAfter) {
+                where = { ...where, updateAt: {
+                    [Op.gte]: ParseIso(updatedAfter),
+                } };
+            }
+    
+            if (sort) {
+                order = sort.split(',').map(item => item.split(':'));
+            }
+    
+            try {
+                const data = await User.findAll({
+                    attributes: { exclude: ['password', 'password_hash'] },
+                    where,
+                    order,
+                    limit,
+                    offset: limit * page - limit,
+                });
+
+                return res.json(data);
+            } catch (err) {
+                return res.status(500).json({
+                    error: err?.message,
+                    original: err?.original,
+                });
+            }
+        }
+    
+        // Show a specific customer by ID
+        //  localhost:3000/customers/1/contacts
+        async show(req, res) {
+            try {
+
+                const user = await User.findByPk(req.params.id, { exclude: ['password', 'password_hash'] });
+
+                const {id, name, email, createdAt, updatedAt} = user;
+
+                if (!user) {
+                    return res.status(404).json({ error: 'Customer not found' });
+                }
+    
+                return res.json({id, name, email, createdAt, updatedAt}); // Retorna o cliente criado com status 201
+            } catch (err) {
+                return res.status(500).json({
+                    error: err?.message,
+                    original: err?.original,
+                });
+            }
+        }
+    
+        // Create a new customer
+        async create(req, res) {
+            try {
+                const schema = Yup.object().shape({
+                    name: Yup.string().required(),
+                    email: Yup.string().email().required(),
+                    password: Yup.string().required().min(8),
+                    passwordConfirmation: Yup.string().when('password', (password, field) =>
+                        password ? field.required().oneOf([Yup.ref('password')]) : field
+                    ),
+            });
+
+                if (!(await schema.isValid(req.body))) {
+                    return res.status(400).json({ error: 'Validation fails' });
+                }
+
+                // Cria o contato corretamente
+                const {id, name, email, createdAt, updatedAt} = await User.create(req.body);
+
+                return res.status(201).json({id, name, email, createdAt, updatedAt});
+            } catch (err) {
+                // Trata erro de unique constraint como fallback
+                if (err.original?.code === '23505') {
+                    return res.status(409).json({
+                        error: 'Esse usuário já existe.',
+                    });
+                }
+
+                return res.status(500).json({
+                    error: err.message,
+                    original: err.original,
+                });
+            }
+        }
+    
+        // Update an existing customer by ID
+        async update(req, res) {
+            try {
+                const schema = Yup.object().shape({
+                    name: Yup.string(),
+                    email: Yup.string().email(),
+                    oldPassword: Yup.string().min(8),
+                    password: Yup.string().min(8).when('oldPassword', (oldPassword, field) =>
+                        oldPassword ? field.required() : field
+                    ),
+                    passwordConfirmation: Yup.string().when('password', (password, field) =>
+                        password ? field.required().oneOf([Yup.ref('password')]) : field
+                    ),
+            });
+
+                if (!(await schema.isValid(req.body))) {
+                    return res.status(400).json({ error: 'Validation fails' });
+                }
+
+                const user = await User.findByPk(req.params.id);
+
+                if (!user) {
+                    return res.status(404).json({ error: 'User not found' });
+                }
+
+                const { oldPassword } = req.body;
+
+                if (oldPassword && !(await user.checkPassword(oldPassword))) {
+                    return res.status(401).json({ error: 'Password does not match' });
+                }
+
+                // Atualiza o user
+                const {id, name, email, createdAt, updatedAt} = await user.update(req.body);
+
+                return res.status(201).json({id, name, email, createdAt, updatedAt});
+            } catch (err) {
+                // Trata erro de unique constraint como fallback
+                if (err.original?.code === '23505') {
+                    return res.status(409).json({
+                        error: 'Esse usuário já existe.',
+                    });
+                }
+
+                return res.status(500).json({
+                    error: err.message,
+                    original: err.original,
+                });
+            }
+        }  
+    
+        // Delete a customer by ID
+        async delete(req, res) {
+            try {
+            const user = await User.findByPk(req.params.id);
+
+            if (!user) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+
+            await user.destroy();
+
+            return res.status(204).send();
+        } catch (err) {
+            return res.status(500).json({
+                error: err?.message,
+                original: err?.original,
+            });
+        }
+    }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 import { Router } from 'express'
 import customers from './app/controllers/customersController'
 import contacts from './app/controllers/contactController'
+import users from './app/controllers/userController'
 
 const routes = new Router();
-
-
 
 // Define routes here
 routes.get("/customers", customers.index);
@@ -19,5 +18,13 @@ routes.get("/customers/:customerId/contacts/:id", contacts.show);
 routes.post("/customers/:customerId/contacts", contacts.create);
 routes.put("/customers/:customerId/contacts/:id", contacts.update);
 routes.delete("/customers/:customerId/contacts/:id", contacts.delete);
+
+// Define routes here
+routes.get("/users", users.index);
+routes.get("/users/:id", users.show);
+routes.post("/users", users.create);
+routes.put("/users/:id", users.update);
+routes.delete("/users/:id", users.delete);
+
 
 export default routes;
