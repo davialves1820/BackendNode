af61824c93377eff029db35a09af75bd8caa4f06
Davi Alves Rodrigues <davialvesr18@gmail.com>
2025-12-15 09:25:22 -0300

    Github workflow

--- jest.setup.js
+++ jest.setup.js
@@ -1,16 +1,11 @@
+import { jest } from "@jest/globals";
+
 jest.mock("./src/config/multer.js", () => ({
     __esModule: true,
     default: {
         storage: {
-            _handleFile(req, file, cb) {
-                cb(null, {
-                    buffer: Buffer.from("fake"),
-                    size: 4,
-                });
-            },
-            _removeFile(req, file, cb) {
-                cb(null);
-            },
+            _handleFile: jest.fn(),
+            _removeFile: jest.fn(),
         },
     },
 }));
