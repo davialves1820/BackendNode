5b790a7f967d0de35191c4ab183e997422bbb21d
Davi Alves Rodrigues <davialvesr18@gmail.com>
2025-12-15 09:09:02 -0300

    Github workflow

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,16 @@
 module.exports = {
     testEnvironment: "node",
 
-    extensionsToTreatAsEsm: [".js"],
-
-    moduleNameMapper: {
-        "^(\\.{1,2}/.*)\\.js$": "$1",
-    },
-
     globalSetup: "<rootDir>/tests/setup.js",
     globalTeardown: "<rootDir>/tests/teardown.js",
 
     setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+
+    transform: {},
+
+    extensionsToTreatAsEsm: [],
+
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
 };
